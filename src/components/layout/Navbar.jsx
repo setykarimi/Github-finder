5996af1f8b3d4867d01f0e678d31d5ada1f67321
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-01-13 20:44:36 +0330

    Edit navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,5 +1,38 @@
-const Navbar = () => {
-    return ( <></> );
+import { FaGithub } from 'react-icons/fa';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const Navbar = ({ title }) => {
+    return (
+        <div className='navbar mb-12 shadow-lg bg-neutral text-neutral-content'>
+            <div className="container mx-auto">
+                <div className="flex-none px-2 mx-2">
+                    <FaGithub className='inline pr-2 text-3xl' />
+                    <Link className='text-lg font-bold' to='/'>
+                        {title}
+                    </Link>
+                </div>
+
+                <div className="flex-1 px-2 mx-2">
+                    <div className="flex justify-end">
+                        <Link to='/' className='btn btn-qhost btn-sm rounded-btn'>
+                            Home
+                        </Link>
+                        <Link to='/about' className='btn btn-qhost btn-sm rounded-btn'>
+                            About
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
- 
+
+Navbar.defaultProps = {
+    title: 'Github Finder'
+}
+
+Navbar.propTypes = {
+    title: PropTypes.string
+}
 export default Navbar
