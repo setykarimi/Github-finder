6888bd49c11130e70e07adf1dfb410afd84da957
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-01-13 21:21:34 +0330

    Add home and about page routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/layout/Footer";
 import Navbar from "./components/layout/Navbar";
+import AboutPage from "./pages/About";
+import HomePage from "./pages/Home";
+import Notfound from "./pages/Notfound";
 
 
 function App() {
   return (
     <BrowserRouter>
-    <div className="flex flex-col justify-between h-screen">
-    <Navbar />
-    <main className="container mx-auto px-3 pb-12">
-      content
-    </main>
-    <Footer />
-    </div>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route path="/" element={<HomePage />}/>
+            <Route path="/about" element={<AboutPage />}/>
+            <Route path="/" element={<Notfound />}/>
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
