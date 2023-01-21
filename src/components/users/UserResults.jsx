607304156c8077fb253e1f1a17f32cb34c6b6277
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-01-21 20:45:54 +0330

    Add UserItem for mapping items

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import UserItem from "./UserItem";
 
 const UserResults = () => {
     const [loading, setLoading] = useState(true);
@@ -10,9 +11,9 @@ const UserResults = () => {
 
     const fetchUsers = async () => {
         const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-            headers: {
-                Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
-            }
+            // headers: {
+            //     Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
+            // }
         })
 
         const data = await response.json()
@@ -25,7 +26,7 @@ const UserResults = () => {
         return ( 
             <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
                 {users.map((user) => (
-                    <h3>{user.login}</h3>
+                    <UserItem key={user.id} user={user}/>
                 ))}
             </div>
          );
