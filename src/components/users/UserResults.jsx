aedbe01efb6be208558081da9c03fb38f47b8558
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-01-23 20:34:26 +0330

    Edit file

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -2,17 +2,13 @@ import { useEffect } from "react";
 import { useGithub } from "../context/github/GithubContext";
 import UserItem from "./UserItem";
 
-const UserResults = () => {
-    
+const UserResults = () => { 
     const {users, loading, fetchUsers} = useGithub()
 
     useEffect(() => {
         fetchUsers()
     },[])
 
-     
-
-   
     if(!loading){
         return ( 
             <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
