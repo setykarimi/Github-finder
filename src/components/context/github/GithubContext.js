4ddd0480d8c36b78f70644b33d2e2c62e13c314c
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-01-23 20:12:37 +0330

    Create new folder for contexts => GithubContext

---
+++ src/components/context/github/GithubContext.js
@@ -0,0 +1,28 @@
+import { createContext, useContext, useState } from "react";
+
+const GithubContext = createContext();
+
+const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
+
+export const GithubProvider = ({children}) => {
+    const [loading, setLoading] = useState(true);
+    const [users, setUsers] = useState([]);
+
+    const fetchUsers = async () => {
+        const response = await fetch(`${GITHUB_URL}/users`, {
+            // headers: {
+            //     Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
+            // }
+        })
+
+        const data = await response.json()
+        setUsers(data)
+        setLoading(false)
+    }
+
+    return <GithubContext.Provider value={{users, loading, fetchUsers}}>
+        {children}
+    </GithubContext.Provider>
+}
+
+export const useGithub = () => useContext(GithubContext)
