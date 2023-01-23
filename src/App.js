6241ade47da1ad96aabee8f3f7126d99a45d5b2e
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-01-23 20:13:02 +0330

    Add GithubContext in app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { GithubProvider } from "./components/context/github/GithubContext";
 import Footer from "./components/layout/Footer";
 import Navbar from "./components/layout/Navbar";
 import AboutPage from "./pages/About";
@@ -8,19 +9,21 @@ import Notfound from "./pages/Notfound";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="flex flex-col justify-between h-screen">
-        <Navbar />
-        <main className="container mx-auto px-3 pb-12">
-          <Routes>
-            <Route path="/" element={<HomePage />}/>
-            <Route path="/about" element={<AboutPage />}/>
-            <Route path="/" element={<Notfound />}/>
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </BrowserRouter>
+    <GithubProvider>
+      <BrowserRouter>
+        <div className="flex flex-col justify-between h-screen">
+          <Navbar />
+          <main className="container mx-auto px-3 pb-12">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/about" element={<AboutPage />} />
+              <Route path="/" element={<Notfound />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </BrowserRouter>
+    </GithubProvider>
   );
 }
 
