c8b9d40c63bca3647f47d9590dc666a6c98cc035
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-01-25 21:31:29 +0330

    Add submitHandler and user from useGithub context

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,19 +1,30 @@
 import { useState } from "react";
+import { useGithub } from "../context/github/GithubContext";
 
 const UserSearch = () => {
-    const [text,setText] = useState('');
+    const [text, setText] = useState('');
+    const { users } = useGithub()
 
     const handleChange = (e) => setText(e.target.value)
-    return ( 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (text === '') {
+            alert('Please enter something')
+        } else {
+            // @todo search users
+            setText('')
+        }
+    }
+    return (
         <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8">
             <div>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="form-control">
                         <div className="relative">
-                            <input type="text" className="w-full pr-40 bg-gray-200 input input-lg text-black" 
-                            placeholder="Search"
-                            value={text}
-                            onChange={handleChange}/>
+                            <input type="text" className="w-full pr-40 bg-gray-200 input input-lg text-black"
+                                placeholder="Search"
+                                value={text}
+                                onChange={handleChange} />
                             <button type="Submit" className="absolute top-0 right-0 rounded-l-none w-36 btn btn-lg">
                                 Go
                             </button>
@@ -21,13 +32,15 @@ const UserSearch = () => {
                     </div>
                 </form>
             </div>
-            <div>
-                <button className="btn btn-ghost btn-lg">
-                    Clear
-                </button>
-            </div>
+            {users.length > 0 && (
+                <div>
+                    <button className="btn btn-ghost btn-lg">
+                        Clear
+                    </button>
+                </div>
+            )}
         </div>
-     );
+    );
 }
- 
+
 export default UserSearch;
