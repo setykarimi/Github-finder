a4cf02c8fb3690ae170a2b984d49956756679f87
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-01-26 14:33:20 +0330

    Define clear userFunction

--- src/components/context/github/GithubContext.js
+++ src/components/context/github/GithubContext.js
@@ -14,26 +14,35 @@ export const GithubProvider = ({ children }) => {
     const [state, dispatch] = useReducer(githubReucer, initialState)
 
     
-    const fetchUsers = async () => {
+    const searchUsers = async (text) => {
+        const params = new URLSearchParams({
+            q: text
+        })
         setLoading()
-        const response = await fetch(`${GITHUB_URL}/users`, {
+        const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
             // headers: {
             //     Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
             // }
         })
 
-        const data = await response.json()
+        const {items} = await response.json()
         dispatch({
             type: 'GET_USERS',
-            payload: data
+            payload: items
         })
+    }
+
+    const clearUsers = () => {
+        dispatch({type: 'CLEAR_USERS'})
     }
 
     const setLoading = () => dispatch({type: 'SET_LOADING'})
 
     return <GithubContext.Provider value={{
         users: state.users,
-        loading: state.loading, fetchUsers
+        loading: state.loading, 
+        searchUsers,
+        clearUsers
     }}>
         {children}
     </GithubContext.Provider>
