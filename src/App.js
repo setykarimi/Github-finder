8afc7f89d92df3752717d08a9df5871214e7d090
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-01-29 21:42:06 +0330

    Add Alert component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AlertProvider } from "./components/context/alert/AlertContext";
 import { GithubProvider } from "./components/context/github/GithubContext";
+import Alert from "./components/layout/Alert";
 import Footer from "./components/layout/Footer";
 import Navbar from "./components/layout/Navbar";
 import AboutPage from "./pages/About";
@@ -10,22 +11,23 @@ import Notfound from "./pages/Notfound";
 
 function App() {
   return (
-
     <GithubProvider>
       <AlertProvider>
-        <BrowserRouter>
-          <div className="flex flex-col justify-between h-screen">
-            <Navbar />
-            <main className="container mx-auto px-3 pb-12">
-              <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/about" element={<AboutPage />} />
-                <Route path="/" element={<Notfound />} />
-              </Routes>
-            </main>
-            <Footer />
-          </div>
-        </BrowserRouter>
+        {/* <Alert> */}
+          <BrowserRouter>
+            <div className="flex flex-col justify-between h-screen">
+              <Navbar />
+              <main className="container mx-auto px-3 pb-12">
+                <Routes>
+                  <Route path="/" element={<HomePage />} />
+                  <Route path="/about" element={<AboutPage />} />
+                  <Route path="/" element={<Notfound />} />
+                </Routes>
+              </main>
+              <Footer />
+            </div>
+          </BrowserRouter>
+        {/* </Alert> */}
       </AlertProvider>
     </GithubProvider>
   );
