e405103013ee11cf2f69d75feb74a4f349c7e685
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-02-04 13:24:33 +0330

    Edit classNames

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -1,7 +1,8 @@
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types'
 const UserItem = ({ user: { login, avatar_url } }) => {
-    return (<div className='card shadow-md compact side bg-base-100'>
+    return (
+    <div className='card shadow-md compact side bg-base-300 hover:text-white'>
         <div className="flex-row items-center space-x-4 card-body">
             <div>
                 <div className="avatar">
