6ae28549ffbfc41ba63659e6820b065408a49c96
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-02-09 20:22:16 +0330

    Edit classNames for responsive view

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,4 +1,4 @@
-import { ArrowCircleLeft, ArrowDown2, ArrowLeft, Profile } from "iconsax-react";
+import { ArrowCircleLeft, ArrowDown2, ArrowLeft, Location, LocationCross, LocationMinus, Profile } from "iconsax-react";
 import { useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useGithub } from "../components/context/github/GithubContext";
@@ -18,83 +18,89 @@ const User = () => {
         return <div>loading</div>
     }
     return (
-        <>
-            <div className="w-full mx-auto lg:w-10/12" >
-                <div className="mb-4">
-                    <Link to='/' className='flex items-center text-sm w-fit'>
-                        <ArrowLeft className="mr-2 text-sm" size="15" />  Back to Search
-                    </Link>
-                </div>
+        <div className="w-full mx-auto lg:w-10/12" >
+            <div className="mb-4">
+                <Link to='/' className='flex items-center text-sm w-fit'>
+                    <ArrowLeft className="mr-2 text-sm" size="15" />  Back to Search
+                </Link>
+            </div>
 
-                <div className="flex">
-                    <figure>
-                        <img src={avatar_url} alt="" className="rounded-full" />
-                    </figure>
-                    <div>
-                        <div className="flex items-center justify-between">
-                            <div>
-                                <h2 className="card-title	font-bold text-lg">{name}</h2>
+            <div className="grid grid-cols-4 gap-5 border p-4 rounded-lg">
+                <figure className="flex flex-col items-center">
+                    <img src={avatar_url} alt="" className="rounded-full" />
+                    <a className="link link-secondary text-xs mt-3" href={html_url} target='_blank' rel="noreferrer">
+                        Visit Github Profile
+                    </a>
+                </figure>
+
+                <div className="col-span-3">
+                    <div className="flex items-center justify-between">
+                        <div>
+                            <h2 className="card-title font-bold text-lg">{name}</h2>
+                            <div className="flex w-full mt-2">
                                 <span className="text-base-content text-opacity-40">{login}</span>
+                                <div className="divider divider-horizontal mx-2"></div>
+                                {
+                                    location &&
+                                    <div className="flex gap-1 items-center text-base-content text-opacity-40">
+                                        <Location size={15} />
+                                        <span>{location}</span>
+                                    </div>
+                                }
                             </div>
-                            <div className="badge px-4 py-3 badge-accent">
+                        </div>
+                        <div className="flex gap-2">
+                            <div className="badge badge-accent">
                                 {type}
                             </div>
-                        </div>
-
-                        {hireable &&
-                            <div className="mx-1 badge badge-info">
-                                Hirebale
-                            </div>}
-                        <p>{bio}</p>
-                        <a className="btn btn-" href={html_url} target='_blank' rel="noreferrer">
-                            Visit Github Profile
-                        </a>
-
-
-                        {location && <div className="stat">
-                            {location}</div>}
-                    </div>
-                </div>
-
-
-
-                {blog && <div><div className="stat">
-                    {blog}</div>
-                    <a href={`https://${blog}`} target='_blank' rel="noreferrer">blog</a></div>
-                }
-
-                {twitter_username && <div><div className="stat">
-                    {blog}</div>
-                    <a href={`https://twitter.com/${twitter_username}`} target='_blank' rel="noreferrer">{twitter_username}</a></div>
-                }
-
-                <div className="">
-                    <div className="">
-                        <div className="">
-                            <h2>Followers</h2>
-                            <span>{followers}</span>
-                        </div>
-
-                        <div className="">
-                            <h2>Following</h2>
-                            <span>{following}</span>
-                        </div>
-
-                        <div className="">
-                            <h2>Public Repos</h2>
-                            <span>{public_repos}</span>
-                        </div>
-
-                        <div className="">
-                            <h2>Public Gist</h2>
-                            <span>{public_gist}</span>
+                            {
+                                hireable &&
+                                <div className="badge badge-info">
+                                    Hirebale
+                                </div>
+                            }
                         </div>
                     </div>
-
-                    <RepoList repos={repos} />
+                    <p className="mt-2">{bio}</p>
+                    {blog &&
+                        <a className="link link-info mt-1 block" href={`https://${blog}`} target='_blank' rel="noreferrer">{blog}</a>
+                    }
                 </div>
             </div>
-        </>);
+
+
+            {
+                twitter_username &&
+                <a href={`https://twitter.com/${twitter_username}`} target='_blank' rel="noreferrer">{twitter_username}</a>
+            }
+
+            
+                <div className="">
+                    <div className="">
+                        <h2>Followers</h2>
+                        <span>{followers}</span>
+                    </div>
+
+                    <div className="">
+                        <h2>Following</h2>
+                        <span>{following}</span>
+                    </div>
+
+                    <div className="">
+                        <h2>Public Repos</h2>
+                        <span>{public_repos}</span>
+                    </div>
+
+                    <div className="">
+                        <h2>Public Gist</h2>
+                        <span>{public_gist}</span>
+                    </div>
+                </div>
+
+                <RepoList repos={repos} />
+           
+        </div>
+    );
 }
 
 export default User;
