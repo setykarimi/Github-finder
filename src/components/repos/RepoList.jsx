38cb60844e7824502a6d4901492007f7aed2fab0
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-02-09 20:22:32 +0330

    Edit repolist

--- src/components/repos/RepoList.jsx
+++ src/components/repos/RepoList.jsx
@@ -2,10 +2,11 @@ import PropTypes from 'prop-types'
 import RepoItem from './RepoItem'
 
 
-const RepoList = ({repos}) => {
-    return ( <div>
-        {repos.map((repo)=> <RepoItem key={repo.id} repo={repo}/>)}
-    </div> );
+const RepoList = ({ repos }) => {
+    return (
+        <div>
+            {repos.map((repo) => <RepoItem key={repo.id} repo={repo} />)}
+        </div>);
 }
 
 RepoList.propTypes = {
