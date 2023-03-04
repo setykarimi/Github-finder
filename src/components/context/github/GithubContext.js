9396f322d6ca70969f96e3aa470314ae2cde1690
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-03-04 21:23:18 +0330

    Remove getUser and getUserRepo from github context

--- src/components/context/github/GithubContext.js
+++ src/components/context/github/GithubContext.js
@@ -15,83 +15,36 @@ export const GithubProvider = ({ children }) => {
 
     const [state, dispatch] = useReducer(gtihubReducer, initialState)
 
+   
 
-    // const searchUsers = async (text) => {
-    //     const params = new URLSearchParams({
-    //         q: text
-    //     })
-    //     setLoading()
-    //     const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-    //         // headers: {
-    //         //     Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
-    //         // }
-    //     })
-
-    //     const { items } = await response.json()
-    //     dispatch({
-    //         type: 'GET_USERS',
-    //         payload: items
-    //     })
-    // }
-
-    // Get single user
-    const getUser = async (login) => {
-        setLoading()
-        const response = await fetch(`${GITHUB_URL}/users/${login}`, {
-            // headers: {
-            //     Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
-            // }
-        })
-
-        if (response.status === 400) {
-            window.location = '/notfound'
-        } else {
-            const data = await response.json()
-            dispatch({
-                type: 'GET_USER',
-                payload: data
-            })
-        }
-
-    }
-
-    // Get user repos
-    const getUserRepos = async (login) => {
-        setLoading()
-        const params = new URLSearchParams({
-            sort: 'created',
-            per_page: 10
-        })
-
-        const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
-            // headers: {
-            //     Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
-            // }
-        })
-
-        const data = await response.json()
-        dispatch({
-            type: 'GET_REPOS',
-            payload: data
-        })
-    }
-
-    const clearUsers = () => {
-        dispatch({ type: 'CLEAR_USERS' })
-    }
-
-    const setLoading = () => dispatch({ type: 'SET_LOADING' })
 
     return <GithubContext.Provider value={{
         ...state,
         dispatch,
         // searchUsers,
-        clearUsers,
-        getUser,
-        getUserRepos
     }}>
         {children}
     </GithubContext.Provider>
 }
 
 export const useGithub = () => useContext(GithubContext)
+
+// const searchUsers = async (text) => {
+//     const params = new URLSearchParams({
+//         q: text
+//     })
+//     setLoading()
+//     const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
+//         // headers: {
+//         //     Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
+//         // }
+//     })
+
+//     const { items } = await response.json()
+//     dispatch({
+//         type: 'GET_USERS',
+//         payload: items
+//     })
+// }
+
+// Get single user
