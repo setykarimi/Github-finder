e50d8f558a2d3c65042814bb66fd4662f5cb25e8
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-03-09 16:30:09 +0330

    Add new class for styling

--- src/components/repos/RepoList.jsx
+++ src/components/repos/RepoList.jsx
@@ -4,8 +4,13 @@ import RepoItem from './RepoItem'
 
 const RepoList = ({ repos }) => {
     return (
-        <div>
-            {repos.map((repo) => <RepoItem key={repo.id} repo={repo} />)}
+        <div className='rounded-lg shadow-lg card bg-base-100 '>
+            <div className="card-body">
+                <h2 className="text-3xl my-4 font-bold card-title">
+                    Latest Repositiories
+                </h2>
+            {/* {repos?.map((repo) => <RepoItem key={repo.id} repo={repo} />)} */}
+            </div>
         </div>);
 }
 
