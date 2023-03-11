d236037bbad4628e9e517e3d031c0ec74458c45c
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-03-11 20:01:22 +0330

    Remove comemnts

--- src/components/context/github/GithubContext.js
+++ src/components/context/github/GithubContext.js
@@ -13,34 +13,12 @@ export const GithubProvider = ({ children }) => {
 
     const [state, dispatch] = useReducer(gtihubReducer, initialState)
 
-
     return <GithubContext.Provider value={{
         ...state,
         dispatch,
-        // searchUsers,
     }}>
         {children}
     </GithubContext.Provider>
 }
 
 export const useGithub = () => useContext(GithubContext)
-
-// const searchUsers = async (text) => {
-//     const params = new URLSearchParams({
-//         q: text
-//     })
-//     setLoading()
-//     const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-//         // headers: {
-//         //     Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
-//         // }
-//     })
-
-//     const { items } = await response.json()
-//     dispatch({
-//         type: 'GET_USERS',
-//         payload: items
-//     })
-// }
-
-// Get single user
